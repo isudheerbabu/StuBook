172bfdca43d46e3991c895e77fdefdf1aa507746
Sudheer <Sudheer.babu@gmail.com>
2020-09-02 17:27:37 +0530

    4 ghpages try

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'StuDaForm'`, () => {
+  it(`should have as title 'StuBook'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('StuDaForm');
+    expect(app.title).toEqual('StuBook');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('StuDaForm app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('StuBook app is running!');
   });
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   firebaseConfig:  {
     apiKey: 'AIzaSyByTqybJTQ1FBTW7gDRYEqJ37b624Ox62E',
-    authDomain: 'studaform.firebaseapp.com',
-    databaseURL: 'https://studaform.firebaseio.com',
-    projectId: 'studaform',
-    storageBucket: 'studaform.appspot.com',
+    authDomain: 'StuBook.firebaseapp.com',
+    databaseURL: 'https://StuBook.firebaseio.com',
+    projectId: 'StuBook',
+    storageBucket: 'StuBook.appspot.com',
     messagingSenderId: '386706194297',
     appId: '1:386706194297:web:8404fe6d5fbf7680a1f9a8',
     measurementId: 'G-HB1F0E2PTP'
